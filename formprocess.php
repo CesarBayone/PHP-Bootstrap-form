a7008e2455ffaae77d6f3f9c0658337423447f55
cesarbayone <cesarbayone402@gmail.com>
2018-12-18 20:37:02 -0400

    Add formprocess.php

---
+++ formprocess.php
@@ -0,0 +1,27 @@
+<?php
+include "db.php";
+	if (isset($_POST["submit_form"])) {
+		$name = strip_tags($_POST["name"]);
+		$email = strip_tags($_POST["email_adress"]);
+		$subject = htmlspecialchars($_POST["subject"]);
+		$gender = $_POST["gender"]);
+		$country = $_POST["country"]);
+		$comment = trim(htmlspecialchars($_POST["comment"]));
+		if (empty($_POST["skill1"])) {
+			$_POST["skill1"] = "";
+		}
+		if (empty($_POST["skill2"])) {
+			$_POST["skill2"] = "";
+		}
+		if (empty($_POST["skill3"])) {
+			$_POST["skill3"] = "";
+		}
+		if (empty($_POST["skill4"])) {
+			$_POST["skill4"] = "";
+		}
+
+		$ins_sql = "INSERT INTO Users_info (name, email_adress, subject, gender, skill1, skill2, skill3, skill4, country, comment) VALUES ('$name', '$email', '$subject', '$gender' ,'$_POST[skill1]', '$_POST[skill2]', '$_POST[skill3]', '$_POST[skill4]', '$country', '$comment')";
+		$run_sql = mysqli_query($conn, $ins_sql);
+
+	} else{ echo "NOT WORKING"};
+?> 
